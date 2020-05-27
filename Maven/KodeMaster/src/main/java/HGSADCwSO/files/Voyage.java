143c7f3a58bccf1190bb39cfe31e9eb662d671fb
Andreas Bakke Moan <abakkem@gmail.com>
2020-05-27 11:34:09 +0200

    Add files via upload

--- Maven/KodeMaster/src/main/java/HGSADCwSO/files/Voyage.java
+++ Maven/KodeMaster/src/main/java/HGSADCwSO/files/Voyage.java
@@ -1,11 +1,27 @@
 package main.java.HGSADCwSO.files;
 
+import main.java.HGSADCwSO.implementations.DAG.Edge;
+import main.java.HGSADCwSO.implementations.DAG.Node;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Voyage {
 
-    private ArrayList<Integer> installations;
+    private Vessel vessel;
+    private Node best_solution_node;
 
-    public ArrayList<Integer> getInstallations() {return installations;}
+    public Voyage(Vessel vessel, Node best_solution_node) {
+        this.vessel = vessel;
+        this.best_solution_node = best_solution_node;
+    }
 
+    public void printVoyage() {
+        ArrayList<Edge> solution_edges = best_solution_node.getAllBestParentEdges();
+        Collections.sort(solution_edges, Utilities.getEdgeTimeComparator());
+        for (int i = 0; i < solution_edges.size(); i++) {
+            Edge edge = solution_edges.get(i);
+            // TODO: Print
+        }
+    }
 }
